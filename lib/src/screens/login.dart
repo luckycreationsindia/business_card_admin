2d51e3bf079b69a268f2c3b161a052029cb87bc1
Lokesh Jain <luckycreationsindia@gmail.com>
2023-08-01 06:44:01 +0530

    *Precheck Login Data

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -23,76 +23,96 @@ class _LoginScreenState extends State<LoginScreen> {
           child: SizedBox(
             height: 600,
             width: 400,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 60.0),
-                  child: Center(
+            child: FutureBuilder<bool>(
+              future: preCheck(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
                     child: SizedBox(
-                      width: 200,
-                      height: 150,
-                      child: Image.asset('assets/images/logo.png'),
+                      height: 50,
+                      width: 50,
+                      child: CircularProgressIndicator(),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(20),
+                  );
+                }
+                if (snapshot.hasData &&
+                    snapshot.data != null &&
+                    snapshot.data!) {
+                  context.go('/dashboard');
+                  return const SizedBox();
+                }
+                return Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60.0),
+                      child: Center(
+                        child: SizedBox(
+                          width: 200,
+                          height: 150,
+                          child: Image.asset('assets/images/logo.png'),
+                        ),
                       ),
-                      labelText: 'Email',
-                      hintText: 'Enter valid email id as abc@gmail.com',
-                      filled: true,
-                      fillColor: const Color(0xFF171B2D),
                     ),
-                    controller: _usernameController,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 15.0,
-                    right: 15.0,
-                    top: 15,
-                    bottom: 0,
-                  ),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          labelText: 'Email',
+                          hintText: 'Enter valid email id as abc@gmail.com',
+                          filled: true,
+                          fillColor: const Color(0xFF171B2D),
+                        ),
+                        controller: _usernameController,
                       ),
-                      labelText: 'Password',
-                      hintText: 'Enter secure password',
-                      filled: true,
-                      fillColor: const Color(0xFF171B2D),
                     ),
-                    controller: _passwordController,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                isLoading
-                    ? const CircularProgressIndicator()
-                    : FilledButton(
-                        onPressed: () {
-                          setState(() {
-                            isLoading = true;
-                            initLogin().then((value) {
-                              context.go('/dashboard');
-                            }).catchError((err) {
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 15.0,
+                        right: 15.0,
+                        top: 15,
+                        bottom: 0,
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          labelText: 'Password',
+                          hintText: 'Enter secure password',
+                          filled: true,
+                          fillColor: const Color(0xFF171B2D),
+                        ),
+                        controller: _passwordController,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    isLoading
+                        ? const CircularProgressIndicator()
+                        : FilledButton(
+                            onPressed: () {
                               setState(() {
-                                isLoading = false;
+                                isLoading = true;
+                                initLogin().then((value) {
+                                  context.go('/dashboard');
+                                }).catchError((err) {
+                                  setState(() {
+                                    isLoading = false;
+                                  });
+                                });
                               });
-                            });
-                          });
-                        },
-                        child: const Text('Login'),
-                      ),
-                const SizedBox(height: 130),
-              ],
+                            },
+                            child: const Text('Login'),
+                          ),
+                    const SizedBox(height: 130),
+                  ],
+                );
+              },
             ),
           ),
         ),
@@ -106,6 +126,11 @@ class _LoginScreenState extends State<LoginScreen> {
       password: _passwordController.text,
     );
     await UserRestClient(Consts.dio).login(login);
+    Consts.USER_DATA = await UserRestClient(Consts.dio).profile();
+    return Future.value(true);
+  }
+
+  Future<bool> preCheck() async {
     Consts.USER_DATA = await UserRestClient(Consts.dio).profile();
     return Future.value(true);
   }
