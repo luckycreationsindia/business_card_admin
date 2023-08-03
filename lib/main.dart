17aa1d2aed8bfc3d309fdcffa4ad2265ed4ac27c
Lokesh Jain <luckycreationsindia@gmail.com>
2023-08-03 08:25:23 +0530

    *Fix Environment Variable for production

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:business_card_admin/m_common.dart';
-import 'package:business_card_admin/utils.dart';
+import 'package:business_card_admin/consts.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'm_app.dart' if(dart.library.html) 'm_web.dart' as dio;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   String env = ".env";
-  if(kDebugMode) env = "production.env";
+  if(!kDebugMode) env = "production.env";
 
   await dotenv.load(fileName: env);
   Consts.env = dotenv.env;
-  WidgetsFlutterBinding.ensureInitialized();
   await dio.initDio();
   runApp(const MyApp());
 }
